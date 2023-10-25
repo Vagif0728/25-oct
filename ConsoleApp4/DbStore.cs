933d43f77f0a84d7c7568142d06151f439781c32
ElnaraHajibalayeva97 <63la1z0@code.edu.az>
2023-10-25 12:57:49 +0400

    new task

---
+++ ConsoleApp4/DbStore.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class DbStore
+    {
+        Book book = new Book("sgggs", "shsgsg", 2000);
+
+        List<Book> books=new List<Book>(5);
+        public void Add(Book book)
+        {
+            book.Id= Guid.NewGuid().ToString();
+
+            books.Add(book);
+           
+        }
+
+        public void AddRange()
+        {
+            books.AddRange(books);
+        }
+        public void Remove(Book book)
+        {
+            books.Remove(book);
+        }
+        public void Update(string id)
+        {
+            foreach (Book book in books) 
+            {
+                if(book.Id == id)
+                {
+                    book.Name = Console.ReadLine();
+                }
+               
+            }
+        }
+        public void ShowBook(string id)
+        {
+            foreach (Book book in books)
+            {
+                if (book.Id == id)
+                {
+                    Console.WriteLine($"{book.Id},{book.Name},{book.Author},{book.Year}");
+                }
+                
+            }
+        }
+        public void  ShowAllBooks()
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine($"{book.Id},{book.Name},{book.Author},{book.Year}");
+            }
+        }
+
+        private List<Book> ReadFromDb()
+        {
+            return books;   
+        }
+
+        private void WriteToDb(List<Book> books)
+        {
+            
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            List<Book> books = new List<Book>();
+
+            Book book = new Book("Xezer2", "Sehriyar1", 2001);
+            Book book2 = new Book("Xezer3", "Sehriyar2", 2002);
+            Book book3 = new Book("Xezer4", "Sehriyar3", 2003);
+
+            DbStore dbStore = new DbStore();
+
+            dbStore.Add(book);
+            dbStore.Add(book2);
+            dbStore.Add(book3);
+
+            dbStore.ShowBook("37c322ec-44b5-4316-9bb1-5dce85934d42");
+            //dbStore.Update("2");
+            //dbStore.ShowAllBooks();
+
+            //Console.WriteLine(book2.Name);
+        }
+    }
+}
